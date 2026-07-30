116a2eb96b4ec9ccbe9bb0cdbf9b9ca4b6607205
AnuragTheCoder <129210052+AnuragTheCoder@users.noreply.github.com>
2026-07-30 21:08:10 +0530

    ADV055-062

--- backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
+++ backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-07-30T20:36:45+0530",
+    date = "2026-07-30T21:04:07+0530",
     comments = "version: 1.6.2, compiler: javac, environment: Java 25.0.2 (Oracle Corporation)"
 )
 @Component
